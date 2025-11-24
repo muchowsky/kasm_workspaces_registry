40b22bafd25fb193fe9377ee5aba7674ea6c7d19
muchowsky <36521001+muchowsky@users.noreply.github.com>
2025-11-24 23:58:09 +0100

    Update basePath in next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     contactUrl: 'https://github.com/muchowsky/kasm_workspaces_registry/issues',
   },
   reactStrictMode: true,
-  basePath: '/workspaces_registry_template/1.0',
+  basePath: '/kasm_workspaces_registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
